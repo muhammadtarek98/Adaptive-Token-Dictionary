5e46a84fb52a2d9cb07bb0c271226754b0b25717
muahmmad <muahmmadtarek@git>
2024-09-11 00:05:07 +0300

    change build MAMBA

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue Mar  5 05:49:10 2024
+# TIME: Tue Sep 10 01:30:33 2024
 __version__ = '1.4.2'
-__gitsha__ = 'unknown'
+__gitsha__ = '01c1aad'
 version_info = (1, 4, 2)
